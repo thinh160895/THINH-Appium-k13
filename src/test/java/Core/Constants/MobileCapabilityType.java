93edf5e24fa93d698132635015ce7ecc1903a562
thinh160895 <thinh.tran@techsoft.asia>
2024-09-01 14:22:02 +0700

    Update pom.xml and redesign Core to POM

---
+++ src/test/java/Core/Constants/MobileCapabilityType.java
@@ -0,0 +1,10 @@
+package Core.Constants;
+
+public interface MobileCapabilityType {
+        String FLATFORM_NAME =  "platformName";
+        String AUTOMATION_NAME =  "automationName";
+        String UDID =  "udid";
+        String APP_PACKAGE =  "appPackage";
+        String APP_ACTIVITY =  "appActivity";
+
+}
